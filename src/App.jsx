b2827fe56d0fb5d08d7b1a3e4c49ee4bfd71f8bf
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-14 13:21:37 +0200

    install mongoDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './designs/pages/HomePage';
 import LoginPage from './designs/pages/LoginPage';
-// import ProfilePage from './designs/pages/ProfilePage';
+import ProfilePage from './designs/pages/ProfilePage';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/profile" element={<ProfilePage />} /> */}
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   )
