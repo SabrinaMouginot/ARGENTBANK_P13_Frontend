bb29631b2adfcda179ce789e57b01cc0fe3aefc9
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-16 13:11:17 +0200

    insertion des composants dans LoginPage

--- src/designs/pages/LoginPage.jsx
+++ src/designs/pages/LoginPage.jsx
@@ -1,25 +1,65 @@
+// import '../css/main.css';
+// import argentBankLogo from '/argentBankLogo.png';
+
+// function LoginPage() {
+//   return (
+//     <div>
+//       <nav className="main-nav">
+//         <a className="main-nav-logo" href="/">
+//           <img
+//             className="main-nav-logo-image"
+//             src={argentBankLogo}
+//             alt="Argent Bank Logo"
+//           />
+//           <h1 className="sr-only">Argent Bank</h1>
+//         </a>
+//         <div>
+//           <a className="main-nav-item" href="/login">
+//             <i className="fa fa-user-circle"></i>
+//             Sign In
+//           </a>
+//         </div>
+//       </nav>
+//       <main className="main bg-dark">
+//         <section className="sign-in-content">
+//           <i className="fa fa-user-circle sign-in-icon"></i>
+//           <h1>Sign In</h1>
+//           <form>
+//             <div className="input-wrapper">
+//               <label htmlFor="username">Username</label>
+//               <input type="text" id="username" />
+//             </div>
+//             <div className="input-wrapper">
+//               <label htmlFor="password">Password</label>
+//               <input type="password" id="password" />
+//             </div>
+//             <div className="input-remember">
+//               <input type="checkbox" id="remember-me" />
+//               <label htmlFor="remember-me">Remember me</label>
+//             </div>
+//             <a href="/profile" className="sign-in-button">Sign In</a>
+//             {/* <button className="sign-in-button">Sign In</button> */}
+//           </form>
+//         </section>
+//       </main>
+//       <footer className="footer">
+//         <p className="footer-text">Copyright 2020 Argent Bank</p>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default LoginPage;
+
+
 import '../css/main.css';
-import argentBankLogo from '/argentBankLogo.png';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 function LoginPage() {
   return (
     <div>
-      <nav className="main-nav">
-        <a className="main-nav-logo" href="/">
-          <img
-            className="main-nav-logo-image"
-            src={argentBankLogo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-          <a className="main-nav-item" href="/login">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </a>
-        </div>
-      </nav>
+      <Header />
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
@@ -38,13 +78,10 @@ function LoginPage() {
               <label htmlFor="remember-me">Remember me</label>
             </div>
             <a href="/profile" className="sign-in-button">Sign In</a>
-            {/* <button className="sign-in-button">Sign In</button> */}
           </form>
         </section>
       </main>
-      <footer className="footer">
-        <p className="footer-text">Copyright 2020 Argent Bank</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
