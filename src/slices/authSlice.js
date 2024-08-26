89a43bb399b05e0da55e67d579f99249a251b096
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-26 19:49:37 +0200

    etat initial du authslice : authentification au démarrage : - isAuthenticated pr indiquer la connexion - token pour stocker ap une connexion reussie - loading pour connexion en cours - error pr stocker les msg d erreur si connexion echoue

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,7 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  isAuthenticated: false,
+  token: null,
+  loading: false,
+  error: null,
+};
+
 const authSlice = createSlice({
   name: 'auth',
+  initialState,
   reducers: {}
 });
 
