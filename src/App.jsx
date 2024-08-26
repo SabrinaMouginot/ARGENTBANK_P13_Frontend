84a2ff572978c853705bce898f5992407b5c0510
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-26 03:33:36 +0200

    creation des fichiers slices

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './designs/pages/HomePage';
 import LoginPage from './designs/pages/LoginPage';
 import ProfilePage from './designs/pages/ProfilePage';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </Router>
+    <Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
