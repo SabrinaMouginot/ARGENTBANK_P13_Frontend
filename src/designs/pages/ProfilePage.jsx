558b1bd06b47db0f5413b4c1011cab7b477f1243
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-09-03 14:03:33 +0200

    ProfilePage - useState pour la mis a jour

--- src/designs/pages/ProfilePage.jsx
+++ src/designs/pages/ProfilePage.jsx
@@ -1,9 +1,9 @@
+import { useState, useEffect } from 'react';
 import '../css/main.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import BankAccount from '../components/BankAccount';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { fetchUserData, toggleEditForm } from '../../slices/userSlice';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,6 +12,10 @@ function ProfilePage() {
   const { firstName, lastName, error, isEditing } = useSelector((state) => state.user);
   const token = useSelector((state) => state.auth.token);
   const navigate = useNavigate();
+
+  // State local pour les valeurs du formulaire
+  const [newFirstName, setNewFirstName] = useState(firstName);
+  const [newLastName, setNewLastName] = useState(lastName);
 
   // Déclencher l'appel à fetchUserData lorsqu'un token est présent
   useEffect(() => {
@@ -22,60 +26,67 @@ function ProfilePage() {
     }
   }, [token, navigate, dispatch, error]);
 
-  const submit = (e) => {
-    e.preventDefault()
+  useEffect(() => {
+    setNewFirstName(firstName);
+    setNewLastName(lastName);
+  }, [firstName, lastName]);
 
 
-    dispatch(toggleEditForm())
+
+    const submit = (e) => {
+      e.preventDefault()
+
+
+      dispatch(toggleEditForm())
+    }
+
+    const editMode = () => {
+      dispatch(toggleEditForm())
+    }
+
+    return (
+      <div>
+        <Header />
+        <main className="main bg-dark">
+
+          {
+            !isEditing ? <div className="header">
+
+              <p>Loading...</p>
+
+              <h1>Welcome back<br />{firstName} {lastName}!</h1>
+
+              <button onClick={editMode} className="edit-button">Edit Name</button>
+            </div>
+              :
+              <form onSubmit={submit} >
+                <input type="text" name="firstName" />
+                <input type="text" name='lastName' />
+                <button onClick={editMode} className="edit-button">Cancel</button>
+                <button className="edit-button" type='submit' >Submit</button>
+              </form>
+          }
+          <h2 className="sr-only">Accounts</h2>
+          <BankAccount
+            title="Argent Bank Checking (x8349)"
+            amount="$2,082.79"
+            description="Available Balance"
+          />
+          <BankAccount
+            title="Argent Bank Savings (x6712)"
+            amount="$10,928.42"
+            description="Available Balance"
+          />
+          <BankAccount
+            title="Argent Bank Credit Card (x8349)"
+            amount="$184.30"
+            description="Current Balance"
+          />
+
+        </main>
+        <Footer />
+      </div>
+    );
   }
 
-  const editMode = () => {
-    dispatch(toggleEditForm())
-  }
-
-  return (
-    <div>
-      <Header />
-      <main className="main bg-dark">
-
-        {
-          !isEditing ? <div className="header">
-
-            <p>Loading...</p>
-
-            <h1>Welcome back<br />{firstName} {lastName}!</h1>
-
-            <button onClick={editMode} className="edit-button">Edit Name</button>
-          </div>
-            :
-            <form onSubmit={submit} >
-              <input type="text" name="firstName" />
-              <input type="text" name='lastName' />
-              <button onClick={editMode} className="edit-button">Cancel</button>
-              <button className="edit-button" type='submit' >Submit</button>
-            </form>
-        }
-        <h2 className="sr-only">Accounts</h2>
-        <BankAccount
-          title="Argent Bank Checking (x8349)"
-          amount="$2,082.79"
-          description="Available Balance"
-        />
-        <BankAccount
-          title="Argent Bank Savings (x6712)"
-          amount="$10,928.42"
-          description="Available Balance"
-        />
-        <BankAccount
-          title="Argent Bank Credit Card (x8349)"
-          amount="$184.30"
-          description="Current Balance"
-        />
-
-      </main>
-      <Footer />
-    </div>
-  );
-}
-
-export default ProfilePage;
+  export default ProfilePage;
